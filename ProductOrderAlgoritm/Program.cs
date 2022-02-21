2bc2eda3bf05c56a81a6b9f3348d1ac6a6c97912
Brand, Thijmen T.A <thijmen.brand@student.fontys.nl>
2022-02-22 00:01:21 +0100

    first-assignment

---
+++ ProductOrderAlgoritm/Program.cs
@@ -0,0 +1,19 @@
+﻿using ProductOrderAlgoritm;
+
+Order order = new Order();
+double minPrice = 1.50;
+
+Console.WriteLine(order.GiveMaximumPrice());
+Console.WriteLine(order.GiveAveragePrice());
+
+Console.WriteLine($"Products with price above {minPrice}");
+order.GetAllProducts(minPrice).ForEach(p =>
+{
+    Console.WriteLine($"Name: {p.Name} and Price {p.Price}");
+});
+
+Console.WriteLine($"List of ordered products");
+order.SortProductsByPrice().ForEach(p =>
+{
+    Console.WriteLine($"Name: {p.Name} and Price {p.Price}");
+});
