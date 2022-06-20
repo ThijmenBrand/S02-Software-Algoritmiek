d89301c41689dab391827c25e841c06c0b9c1eed
Brand, Thijmen T.A <thijmen.brand@student.fontys.nl>
2022-06-20 10:02:05 +0200

    added a graph

--- AI chat bot/skip-gram/own-implementation.py
+++ AI chat bot/skip-gram/own-implementation.py
@@ -1,38 +1,36 @@
+from calendar import c
 import numpy as np
-import re
 from collections import defaultdict
 import collections
 from itertools import islice
-import torch
+import matplotlib.pyplot as plt
+import string
+from nltk.corpus import stopwords
 
 class word2vec():
-    def __init__(self):
-        #Number of hidden neurons
+    def __init__(self, epochs):
         self.N = 5
-        #Learning rate
         self.eta = 0.03
-        #Number of epochs
-        self.epochs = 10000
-        #Window size that the model will use to generate the context words
+        self.epochs = epochs
         self.window = 2
 
     def generate_training_data(self, corpus):
-        #Split all the words in the sentances
+        corpus = corpus.split('.')
         corpus = [x.split() for x in corpus]
-        #Get the word count in the corpus
+        
         word_counts = defaultdict(int)
         for row in corpus:
             for word in row:
                 word_counts[word] += 1
 
+        print(corpus)
+
         self.v_count = len(word_counts.keys())
 
-        #Make dictionary of words and their index and revese
         self.words_list = sorted(list(word_counts.keys()),reverse=False)
         self.word_index = dict((word, i) for i, word in enumerate(self.words_list))
         self.index_word = dict((i, word) for i, word in enumerate(self.words_list))
 
-        #Create the training data where you create an array of the context words and the target word
         training_data = []
         for sentence in corpus:
             sent_len = len(sentence)
@@ -46,7 +44,6 @@ class word2vec():
                 training_data.append([w_target, w_context])
         return np.array(training_data)
 
-    #make one hot encoding for a word
     def word2onehot(self, word):
         word_vec = [0 for i in range(0, self.v_count)]
         word_index = self.word_index[word]
@@ -57,8 +54,6 @@ class word2vec():
         e_x = np.exp(x -np.max(x))
         return e_x / e_x.sum(axis=0)
 
-    #op basis van de forward pas en de hidden layer probeert hij de context woorden en voorspellen. Dit is dan ook de output van het model. De hoeveelheid hangt af van de window size.
-    #Daarna wordt de error berekend op basis van de voorspellingen en de context woorden die er in de corpus zitten.
     def forward_pass(self, x):
         h = np.dot(self.w1.T, x)
         u = np.dot(self.w2.T, h)
@@ -73,27 +68,23 @@ class word2vec():
         self.w2 = self.w2 - (self.eta * dl_dw2)
 
     def train(self, training_data):
-        self.w1 = np.random.uniform(-0.8, 0.8, (self.v_count, self.N))     # context matrix
-        self.w2 = np.random.uniform(-0.8, 0.8, (self.N, self.v_count))     # embedding matrix
+        self.w1 = np.random.uniform(-0.8, 0.8, (self.v_count, self.N))
+        self.w2 = np.random.uniform(-0.8, 0.8, (self.N, self.v_count))
 
         for i in range(0, self.epochs):
             self.loss = 0
 
-            #Cycle through the training examples
             for w_t, w_c in training_data:
                 y_pred, h, u = self.forward_pass(w_t)
 
-                #Calcule the error
                 e = np.sum([np.subtract(y_pred, word) for word in w_c], axis=0)
 
                 self.backprop(e, h, w_t)
 
-                #Calculate the loss
                 self.loss += -np.sum([u[word.index(1)] for word in w_c]) + len(w_c) * np.log(np.sum(np.exp(u)))
 
             print("epoch:", i, "Loss:", self.loss)
 
-    # input a word, returns a vector (if available)
     def word_vec(self, word):
         w_index = self.word_index[word]
         v_w = self.w1[w_index]
@@ -122,12 +113,16 @@ class word2vec():
 
         return np.dot(v_w1,v_w2)/(np.linalg.norm(v_w1)*np.linalg.norm(v_w2))
 
+    def plot_vectors(self):
+        for i, word in enumerate(self.words_list):
+            x, y = float(self.w1[i][0]), float(self.w1[i][1])
+            plt.scatter(x,y)
+            plt.annotate(word, xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
+        plt.show()
 
-corpus = [
-    'he is a king', 'he is my father', 'my father is a king', 'a king is my father'
-]
-w2v = word2vec()
+
+corpus = """he is a king. she is a queen. he is a man. she is a woman. paris is the capital of france. the capital of france is paris. the capital of italy is rome. rome is the capital of italy. a queen is she. he is a king and a man"""
+w2v = word2vec(10_000)
 training_data = w2v.generate_training_data(corpus)
 w2v.train(training_data)
-print(w2v.word_sim('father', 2))
-print(w2v.similarity('he', 'father'))
+w2v.plot_vectors()
